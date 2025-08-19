d000f8d4a4ea00bc01361455cbdfc58a6ccaaf2c
ajeevsan <sanjeevmajhi036@gmail.com>
2025-08-19 20:04:09 +0530

    adding main server

--- Client-Server Model and request lifecycle/server.js
+++ Client-Server Model and request lifecycle/server.js
@@ -2,3 +2,25 @@ const express = require('express')
 
 const app = express()
 
+const PORT = 3000
+
+app.use(express.json())
+
+app.get('/', (req, res) => {
+    res.send('Hello from express server.')
+})
+
+app.get('/users', (req, res) => {
+    const users = [{id: 1, name: 'Alice'}, {id: 2, name: 'Bob'}]
+    res.json(users)
+})
+
+app.post('/users', (req, res) => {
+    const newUser = req.body
+    console.log('User received: ', newUser)
+    res.status(201).json({ message: 'User Created', data: newUser})
+})
+
+app.listen(PORT, () => {
+    console.log(`Listning to port ${PORT}`)
+})
